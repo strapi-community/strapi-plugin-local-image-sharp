32ebc58dec18dd33ef7b5a51a51cb69a407b98cd
Sacha STAFYNIAK <sacha@digisquad.io>
2022-04-04 13:32:05 +0200

    fix: custom koa middleware to avoid overriding original url

--- strapi-server.js
+++ strapi-server.js
@@ -1,7 +1,83 @@
 'use strict';
 
+const { decode } = require('ufo')
+const getEtag = require('etag')
 const Router = require('@koa/router');
-const { createIPX, handleRequest  } = require("ipx");
+const { createIPX  } = require("ipx");
+
+async function createMiddleware (ipx) {
+  return async function ipxMiddleware(ctx) {
+    const url = ctx.req.url.replace('/uploads', '').split('?')[0]
+    // Parse URL
+    const [modifiersStr = '', ...idSegments] = url.substr(1 /* leading slash */).split('/')
+    const id = decode(idSegments.join('/'))
+
+    // Validate
+    if (!modifiersStr) {
+      return
+    }
+    if (!id || id === '/') {
+      return
+    }
+
+    // Contruct modifiers
+    const modifiers = Object.create(null)
+
+    // Read modifiers from first segment
+    if (modifiersStr !== '_') {
+      for (const p of modifiersStr.split(',')) {
+        const [key, value = ''] = p.split('_')
+        modifiers[key] = decode(value)
+      }
+    }
+
+    // Create request
+    const img = ipx(id, modifiers, ctx.req.options)
+
+    // Get image meta from source
+    try {
+      const src = await img.src()
+
+      // Caching headers
+      if (src.mtime) {
+        if (ctx.req.headers['if-modified-since']) {
+          if (new Date(ctx.req.headers['if-modified-since']) >= src.mtime) {
+            ctx.status = 304
+            return
+          }
+        }
+        ctx.set('Last-Modified', `${+src.mtime}`)
+      }
+      if (src.maxAge !== undefined) {
+        ctx.set('Cache-Control', `max-age=${+src.maxAge}, public, s-maxage=${+src.maxAge}`)
+      }
+
+      // Get converted image
+      const { data, format } = await img.data()
+
+      // ETag
+      const etag = getEtag(data)
+      ctx.set('ETag', etag)
+      if (etag && ctx.req.headers['if-none-match'] === etag) {
+        ctx.status = 304
+        return
+      }
+
+      // Mime
+      if (format) {
+        ctx.set('Content-Type', `image/${format}`)
+      }
+
+      ctx.body = data
+    } catch (error) {
+      const statusCode = parseInt(error.statusCode, 10) || 500
+      const statusMessage = error.statusMessage ? error.statusMessage : `IPX Error (${statusCode})`
+      strapi.log.debug(statusMessage)
+    
+      ctx.status = statusCode
+    }
+  }
+}
 
 const plugin = {
   bootstrap({ strapi }) {
@@ -9,32 +85,9 @@ const plugin = {
       dir: `${strapi.dirs.dist.public}/uploads`,
     })
     const router = new Router();
+    const middeware = createMiddleware(ipx)
   
-    router.get('/uploads/(.*)', async (ctx) => {
-      try {
-        // eslint-disable-next-line prefer-destructuring
-        ctx.req.url = ctx.req.url.replace('/uploads', '').split('?')[0]
-  
-        const img = await handleRequest(ctx.req, ipx)
-  
-        if (img.statusCode === 200 || img.statusCode === 304) {
-          for (const header in img.headers) {
-            if (Object.prototype.hasOwnProperty.call(img.headers, header)) {
-              ctx.set(header, img.headers[header])
-            }
-          }
-  
-          ctx.status = img.statusCode
-          ctx.body = img.body
-        }
-      } catch(error) {
-        const statusCode = parseInt(error.statusCode, 10) || 500
-        const statusMessage = error.statusMessage ? error.statusMessage : `IPX Error (${statusCode})`
-        strapi.log.error(statusMessage)
-  
-        ctx.status = statusCode
-      }
-    })
+    router.get('/uploads/(.*)', middeware)
   
     strapi.server.router.use(router.routes());
   }
