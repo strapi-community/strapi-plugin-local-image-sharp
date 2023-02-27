508f585bfdf3751eaefd3fdf2499583714283f1b
Candido Sales Gomes <candidosg@gmail.com>
2023-02-27 17:35:48 -0500

    added validate schema config

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,15 @@
 "use strict";
 
+const { pluginConfigSchema } = require("./schema");
+
 const config = {
   default: ({ env }) => ({
     cacheDir: env("STRAPI_PLUGIN_LOCAL_IMAGE_SHARP_CACHE_DIR", ""),
     maxAge: 3600,
   }),
-  validator() {},
+  validator: (config) => {
+    pluginConfigSchema.validateSync(config);
+  },
 };
 
 module.exports = {
