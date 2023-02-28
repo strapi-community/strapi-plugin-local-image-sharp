e1c6b3b788525bcd1ccf42efce3aa7c49788bd13
Candido Sales Gomes <candidosg@gmail.com>
2023-02-27 21:28:40 -0500

    added babel-eslint as devDependency, and fix config

--- src/config/index.js
+++ src/config/index.js
@@ -2,16 +2,12 @@
 
 const { pluginConfigSchema } = require("./schema");
 
-const config = {
+module.exports = {
   default: ({ env }) => ({
     cacheDir: env("STRAPI_PLUGIN_LOCAL_IMAGE_SHARP_CACHE_DIR", ""),
     maxAge: 3600,
   }),
-  validator: (config) => {
+  validator(config) {
     pluginConfigSchema.validateSync(config);
   },
-};
-
-module.exports = {
-  config,
 };
